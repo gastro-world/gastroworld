6b067f60a46bc245f6cb8c4e97990ee3f75e7d43
SciDroid <scidroid@scidroid.me>
2021-12-10 16:35:53 -0500

    feat: :muscle: A lot of cool stuff

---
+++ components/Footer.jsx
@@ -0,0 +1,16 @@
+const Footer = () => {
+    return (
+      <section className="text-gray-700 bg-white body-font">
+        <div className="container flex flex-col items-center px-8 py-8 mx-auto max-w-7xl sm:flex-row">
+          <p className="text-xl font-black leading-none text-gray-900 select-none logo">
+            GastroWorld<span className="text-yellow-400">.</span>
+          </p>
+          <p className="mt-4 text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l sm:border-gray-200 sm:mt-0">
+            © 2021 GastroWorld - The worlds most delicious map
+          </p>
+        </div>
+      </section>
+    );
+  };
+  
+  export default Footer;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import 'tailwindcss/tailwind.css'
+import "tailwindcss/tailwind.css";
+import { UserProvider } from "@auth0/nextjs-auth0";
+import Layout from "../components/Layout";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }) {
+  return (
+    <UserProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </UserProvider>
+  );
 }
 
-export default MyApp
+export default App;
