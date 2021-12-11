e943755c7bc4ed2cbe9dda6f9e9b261d418cfd7a
SciDroid <scidroid@scidroid.me>
2021-12-11 15:53:22 -0500

    feat: Final Commit

--- pages/app/add/[lat]/[lng].js
+++ pages/app/add/[lat]/[lng].js
@@ -6,12 +6,14 @@ import { Widget } from "@uploadcare/react-widget";
 import MarkdownIt from "markdown-it";
 import MdEditor from "react-markdown-editor-lite";
 import "react-markdown-editor-lite/lib/index.css";
+import confetti from "canvas-confetti";
 
 const mdParser = new MarkdownIt();
 
 const Add = () => {
   const [value, setValue] = useState("");
   const [image, setImage] = useState("");
+  const [preview, setPreview] = useState({});
   const router = useRouter();
   const { lat, lng } = router.query;
   const handleEditorChange = ({ html, text }) => {
@@ -20,6 +22,7 @@ const Add = () => {
   const {
     register,
     handleSubmit,
+    getValues,
     formState: { errors },
   } = useForm();
   const onSubmit = async (data) => {
@@ -36,52 +39,99 @@ const Add = () => {
         error: <b>Could not save. :(</b>,
       }
     );
+    confetti({
+      particleCount: 500,
+      spread: 150,
+      origin: { y: 0 },
+    });
+  };
+  const handleChange = () => {
+    let data = getValues();
+    data["lat"] = parseFloat(lat);
+    data["lng"] = parseFloat(lng);
+    data["image"] = image;
+    data["description"] = value;
+    console.log(data);
+    setPreview(data);
   };
 
   return (
-    <div>
+    <>
       <Toaster />
-      <form
-        className="flex flex-col justify-center items-center"
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <input
-          className="appearance-none block w-2/3 bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-          type="text"
-          placeholder="Name"
-          {...register("name", {})}
-        />
-        <input
-          className="appearance-none block w-2/3 bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-          type="text"
-          placeholder="Place"
-          {...register("place", {})}
-        />
-        <Widget
-          publicKey="624de14caf2cf3c7f75c"
-          onChange={(info) => setImage(info.cdnUrl)}
-          id="file"
-          previewStep="true"
-        />
-        <input
-          className="appearance-none block w-2/3 bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-          type="url"
-          placeholder="Youtube Url"
-          {...register("youtube", {})}
-        />
-        <MdEditor
-          style={{ height: "500px" }}
-          renderHTML={(text) => mdParser.render(text)}
-          onChange={handleEditorChange}
-        />
+      <div className="flex flex-row flex-wrap justify-between">
+        <div className="w-3/6">
+          <form
+            className="flex flex-col justify-center items-center"
+            onSubmit={handleSubmit(onSubmit)}
+            onChange={() => handleChange()}
+          >
+            <input
+              className="appearance-none block w-4/5 bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              type="text"
+              placeholder="Name"
+              {...register("name", {})}
+            />
+            <input
+              className="appearance-none block w-4/5 bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              type="text"
+              placeholder="Place"
+              {...register("place", {})}
+            />
+            <Widget
+              publicKey="624de14caf2cf3c7f75c"
+              onChange={(info) => setImage(info.cdnUrl)}
+              id="file"
+              previewStep="true"
+            />
+            <input
+              className="appearance-none block w-4/5 bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              type="url"
+              placeholder="Youtube Url"
+              {...register("youtube", {})}
+            />
+            <MdEditor
+              className="w-5/6 h-96"
+              renderHTML={(text) => mdParser.render(text)}
+              onChange={handleEditorChange}
+              preview="edit"
+            />
 
-        <input
-          className="m-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-2/3"
-          type="submit"
-          value="Submit"
-        />
-      </form>
-    </div>
+            <input
+              className="m-4 bg-blue-500 w-4/5 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="submit"
+              value="Submit"
+            />
+          </form>
+        </div>
+        <div className="w-3/6 flex items-center flex-col ">
+          {preview.image && (
+            <img
+              src={preview.image}
+              alt={"example"}
+              className="w-3/5"
+            />
+          )}{" "}
+          <h1 className="text-center text-4xl font-bold">{preview.name}</h1>
+          <h2 className="text-center text-2xl font-semibold">
+            {preview.place}
+          </h2>
+          {preview.youtube && (
+            <a
+              href={preview.youtube}
+              className="text-base text-blue-400 underline"
+            >
+              View youtube video
+            </a>
+          )}
+          <div className="block">
+            <div
+              className="container-2"
+              dangerouslySetInnerHTML={{ __html: preview.description }}
+            ></div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
--- pages/app/food/[id].js
+++ pages/app/food/[id].js
@@ -3,29 +3,37 @@ import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 
 const Food = ({ food }) => {
   return (
-    <div className="flex flex-col items-center justify-center">
-      <Image
-        src={food.image}
-        alt={food.name}
-        width="900px"
-        height="400px"
-        objectFit="cover"
-      />
-      <h1 className="font-extrabold text-7xl">{food.name}</h1>
-      <h2 className="font-bold text-3xl">{food.place}</h2>
-      <div className="w-2/3 container" dangerouslySetInnerHTML={{ __html: food.description }}></div>
-      <iframe
-        width="900"
-        height="400"
-        src={`https://www.youtube.com/embed/${
-          food.youtube.split("https://www.youtube.com/watch?v=")[1]
-        }`}
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-        title="Embedded youtube"
-      />
-    </div>
+    <>
+      <h1 className="font-extrabold text-5xl m-4 text-center">
+        {food.name} - {food.place}
+      </h1>
+      <div className="flex flex-row justify-between">
+        <div
+          className="w-2/3 m-2 container"
+          dangerouslySetInnerHTML={{ __html: food.description }}
+        ></div>
+        <div className="w-2/3 m-2">
+          <Image
+            src={food.image}
+            alt={food.name}
+            width="900px"
+            height="400px"
+            objectFit="cover"
+          />
+          <iframe
+            width="900"
+            height="400"
+            src={`https://www.youtube.com/embed/${
+              food.youtube.split("https://www.youtube.com/watch?v=")[1]
+            }`}
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+            title="Embedded youtube"
+          />
+        </div>
+      </div>
+    </>
   );
 };
 
